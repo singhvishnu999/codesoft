e0bb5852b94360cc172af57302818f2f9885e63a
singhvishnu999 <vishnussm9693@gmail.com>
2024-06-18 14:22:14 +0530

    project

--- numberGuessing.cpp
+++ numberGuessing.cpp
@@ -1,25 +1,29 @@
-#include<iostream>
+#include <iostream>
 #include <cstdlib>
 using namespace std;
 
-int main(){
-
-    cout<<"*********** Welcome to Number Guessing Game *************"<<endl;
-    int num, count=0;
+int main()
+{
+    cout << "*********** Welcome to Number Guessing Game *************" << endl;
+    int num, count = 0;
     int ans;
-    for(int i = 0; i < 1; i++){
+    for (int i = 0; i <= 1; i++)
+    {
         num = rand() % 1000;
     }
-cout<<rand();
-    cout<<"Enter A number between 0 to 1000: ";
-    while(ans!=num){
-    cin>>ans;
-    count++;
-    if(ans > num) cout<<"enter a small number: ";
-    else if(ans<num) cout<<"enter a large number: ";
-    else break;
+    cout << "Enter A number between 0 to 1000: ";
+    while (ans != num)
+    {
+        cin >> ans;
+        count++;
+        if (ans > num)
+            cout << "enter a small number: ";
+        else if (ans < num)
+            cout << "enter a large number: ";
+        else
+            break;
     }
-    cout<<"Congratulations you have guessed a number in "<<count<< " times! Successfully";
-    
+    cout << "Congratulations you have guessed a number in " << count << " times! Successfully";
+
     return 0;
 }
--- simpleCalc.cpp
+++ simpleCalc.cpp
@@ -1,47 +1,63 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int add(int a, int b){
-    return a+b;
+int add(int a, int b)
+{
+    return a + b;
 }
-int sub(int a, int b){
-    return a-b;
+int sub(int a, int b)
+{
+    return a - b;
 }
-int mul(int a, int b){
-    return a*b;
+int mul(int a, int b)
+{
+    return a * b;
 }
-int divide(int a, int b){
-    return a/b;
+int divide(int a, int b)
+{
+    return a / b;
 }
 
-int main(){
-    cout<<"*******SIMPLE CALCULATOR********"<<endl;
-    cout<<"     ***************      "<<endl;
+int main()
+{
+    cout << "*******SIMPLE CALCULATOR********" << endl;
+    cout << "     ***************      " << endl;
     bool conti = true;
     char ask;
     int a, b;
     char op;
-    while(conti){
-    cout<<"enter operation to perform (+, -, *, /): ";
-    cin>>op;
-    cout<<"Enter two number : ";
-    cin>>a>>b;
-    if(op=='+') cout<<"the sum of " <<a<<" "<<b<<" is "<<add(a,b);
-    else if(op=='-') cout<<"the subtract of " <<a<<" "<<b<<" is "<<sub(a,b);
-    else if(op=='*') cout<<"the multiplication of " <<a<<" "<<b<<" is "<<mul(a,b);
-    else if (op=='/'){
-      if(b!=0) cout<<"the division of " <<a<<" "<<b<<" is "<<divide(a,b)<<" and reminder is "<<(a%b);
-      else cout<<"Number is not divisible by 0 : ";
-      
-    } 
-    else cout<<"You Enter a Invalid operatoin !";
+    while (conti)
+    {
+        cout << "enter operation to perform (+, -, *, /): ";
+        cin >> op;
+        cout << "Enter two number : ";
+        cin >> a >> b;
+        if (op == '+')
+            cout << "the sum of " << a << " " << b << " is " << add(a, b);
+        else if (op == '-')
+            cout << "the subtract of " << a << " " << b << " is " << sub(a, b);
+        else if (op == '*')
+            cout << "the multiplication of " << a << " " << b << " is " << mul(a, b);
+        else if (op == '/')
+        {
+            if (b != 0)
+                cout << "the division of " << a << " " << b << " is " << divide(a, b) << " and reminder is " << (a % b);
+            else
+                cout << "Number is not divisible by 0 : ";
+        }
+        else
+            cout << "You Enter a Invalid operatoin !";
 
-    cout<<endl<<"DO you Want to continue (y/n): ";
-    cin>>ask;
-    if(ask == 'n' || ask=='N') conti = false;
-    else conti=true;
+        cout << endl
+             << "DO you Want to continue (y/n): ";
+        cin >> ask;
+        if (ask == 'n' || ask == 'N')
+            conti = false;
+        else
+            conti = true;
     }
 
-    cout<<"Thank you for using this calculator "<<endl<<"Have a nice day!";
+    cout << "Thank you for using this calculator " << endl
+         << "Have a nice day!";
     return 0;
 }
